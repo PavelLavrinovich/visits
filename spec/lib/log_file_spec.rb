5be2b6e7d8e8509831c45ba03bcef92058c2bd66
PavelLavrinovich <Pavel.Lavrinovich-ext@fidor.com>
2020-09-06 23:06:38 +0300

    LogFile load with invalid lines spec

--- spec/lib/log_file_spec.rb
+++ spec/lib/log_file_spec.rb
@@ -28,7 +28,28 @@ RSpec.describe Lib::LogFile do
     it 'stores the line'
 
     context 'with invalid line' do
-      it 'adds the line to invalid group'
+      let(:log_file) { Lib::LogFile.new('spec/fixtures/webserver_small_with_invalid.log') }
+      let(:expected_visits_data) do
+        [
+          ['/help_page/1', '126.318.035.038'],
+          ['/contact', '184.123.665.067'],
+          ['/about/2', '444.701.448.104'],
+          ['/help_page/1', '929.398.951.889'],
+          ['/index', '444.701.448.104'],
+          ['/help_page/1', '722.247.931.582'],
+          ['/help_page/1', '646.865.545.408'],
+          ['/home', '235.313.352.950'],
+        ]
+      end
+
+      it 'reads file with only valid data' do
+        expect(log_file.load).to eq(expected_visits_data)
+      end
+
+      it 'adds the line to invalid group' do
+        log_file.load
+        expect(log_file.invalid_lines).to eq(['first_invalid_line', 'second_invalid_line'])
+      end
 
       it 'does not store the line'
     end
